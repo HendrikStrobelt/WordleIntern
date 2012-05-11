369b93b65ab8435d6407a85d6f3e95558eeb9e42
Marc Spicker <marc.spicker@uni-konstanz.de>
2012-05-11 13:01:06 +0200

    added FontManager

---
+++ src/de/graphics/uni_konstanz/wordle/FontManager.java
@@ -0,0 +1,21 @@
+package de.graphics.uni_konstanz.wordle;
+
+import java.awt.Font;
+
+public class FontManager {
+
+	private final int minFS;
+	private final int maxFS;
+	private final Font font;
+
+	public FontManager(final int _minFS, final int _maxFS, final String fontName) {
+		minFS = _minFS;
+		maxFS = _maxFS;
+		font = Font.getFont(fontName);
+	}
+
+	public Font get(final double value) {
+		final float f = Math.round((1 - value) * minFS + value * maxFS);
+		return font.deriveFont(f);
+	}
+}
