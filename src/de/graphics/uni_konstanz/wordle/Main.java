001cf8074a41dd2aa9ad69fc6a21fc03eaac95b9
Hendrik Strobelt <hendrik.strobelt@uni-konstanz.de>
2012-05-11 11:12:00 +0200

    blobb

--- src/de/graphics/uni_konstanz/wordle/Main.java
+++ src/de/graphics/uni_konstanz/wordle/Main.java
@@ -1,5 +1,62 @@
 package de.graphics.uni_konstanz.wordle;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Panel;
+
+import javax.swing.*;
+
 public class Main {
+
+	private JFrame fenster;
+
+	public Main() {
+		fenster = new JFrame("Fenster");
+		fenster.setSize(300, 300);
+		fenster.setLocation(300, 300);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setVisible(true);
+		
+		
+		BorderLayout borderLayout = new BorderLayout();
+		JPanel guiPanel = new JPanel();
+		guiPanel.setLayout(new BoxLayout(guiPanel, BoxLayout.Y_AXIS));
+		guiPanel.add(new JButton("yes"));
+		guiPanel.add(new JButton("no"));
+		guiPanel.setMinimumSize(new Dimension(200,0));
+		Panel panel = new Panel(borderLayout);
+		panel.add(guiPanel, BorderLayout.WEST);
+		
+		
+		fenster.add(panel);
+		
+		
+		
+		
+		
+		
+		fenster.pack();
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		
+		
+		
+		
+		
+		String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment()
+				.getAvailableFontFamilyNames();
+
+		for (int i = 0; i < fonts.length; i++) {
+			System.out.println(fonts[i]);
+		}
+	}
 
 }
---
+++ src/de/graphics/uni_konstanz/wordle/WordlePainter.java
@@ -0,0 +1,9 @@
+package de.graphics.uni_konstanz.wordle;
+
+import java.awt.Graphics2D;
+
+public interface WordlePainter {
+
+	void paint(Graphics2D g);
+	
+}
